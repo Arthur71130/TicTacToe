46f44eff2b4344c61d03f198b2efc15a83d57a09
UpnorthSchool <kevin.delorme.2@ens.etsmtl.ca>
2025-10-02 15:18:37 -0400

    modification de main avec methode de base

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        //initialise un move a jouer null
+        Move moveTest = new Move();
+        //initialise un jeu de tictactoe vide.
+        Board test = new Board();
+        test.getBoard();
+        moveTest.setRow(1);
+        moveTest.setCol(1);
+
+
+        test.play(moveTest, Mark.X);
+
+
+        test.getBoard();
+        System.out.println("Hello world!");
     }
 }
